622782bec90bc38e856226580d1977f67af0fc90
Kushal Pendekanti <pendekanti.s@northeastern.edu>
2026-03-18 00:16:33 -0400

    wired up all auth page routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import "./App.css";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <main className="ic-main">
         <Routes>
           <Route path="/" element={<div>Home</div>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </main>
     </div>
